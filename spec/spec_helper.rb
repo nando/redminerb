50332b2847c0c23a2deea7e11bd9905e0f907f6a
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2015-09-28 13:12:56 +0200

    default user template render test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,16 @@ require 'minitest/spec'
 require 'minitest/autorun'
 
 require 'climate_control'
+require 'json'
 
 SPECS_TMP_DIR      = './tmp/specs'
 SPECS_FIXTURES_DIR = './spec/fixtures'
 SPECS_HOME_DIR     = SPECS_FIXTURES_DIR + '/home'
+
+def json_from_fixture(fixture)
+  JSON.parse(File.read(fixture_path("#{fixture}.json")))
+end
+
+def fixture_path(filename)
+  File.join(File.dirname(__FILE__), 'fixtures', filename)
+end
--- spec/template/template_spec.rb
+++ spec/template/template_spec.rb
@@ -10,7 +10,6 @@ describe Redminerb::Template do
     # Fake home pointing to spec/fixtures
     before do
       @real_home = ENV['HOME']
-      ENV['HOME'] = SPECS_FIXTURES_DIR
     end
 
     after do
@@ -20,8 +19,16 @@ describe Redminerb::Template do
     let(:user_id) { 42 }
 
     it 'should use local templates when present' do
+      ENV['HOME'] = SPECS_FIXTURES_DIR # SEE ITS .redminerb SUBDIRECTORY
       user = OpenStruct.new(id: user_id)
       _(Redminerb::Template.render(:user, user).chomp).must_equal user_id.to_s
+    end
+
+    it 'should use gem templates by default' do
+      ENV['HOME'] = SPECS_TMP_DIR
+      FileUtils.rm_rf SPECS_TMP_DIR
+      user = OpenStruct.new(json_from_fixture('user')['user'])
+      _(Redminerb::Template.render(:user, user)).must_include "Id: #{user.id}"
     end
   end
 end
